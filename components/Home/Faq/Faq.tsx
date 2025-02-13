2c7cd4dcd7ae58e23c03316c0d9bde2ac5f66d79
Utkarsh Rana <utkarshranaa06@gmail.com>
2025-02-12 23:12:04 -0500

    added animation

--- components/Home/Faq/Faq.tsx
+++ components/Home/Faq/Faq.tsx
@@ -7,7 +7,7 @@ const Faq = () => {
 <div className="pt-16 pb-16 bg-gray-950">
   <div className="w-[80%] mx-auto mt-16 grid grid-cols-1 md:grid-cols-2 gap-10 items-center">
     {/* Image */}
-    <div>
+    <div data-aos="zoom-out" data-aos-anchor-placement="top-center">
       <Image src="/images/faq.png" alt="faq" width={500} height={500} />
     </div>
 
