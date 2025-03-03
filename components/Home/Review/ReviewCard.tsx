4e111761730a5aab3289d6bfbcc50f05c0788cad
Utkarsh Rana <104561101+utkarshranaa@users.noreply.github.com>
2025-03-03 12:46:41 -0500

    updated ReviewCard.tsx

--- components/Home/Review/ReviewCard.tsx
+++ components/Home/Review/ReviewCard.tsx
@@ -13,10 +13,7 @@ const ReviewCard = ({ image, name, role }: Props) => {
     <div className="flex items-center justify-center flex-col">
       <QuoteIcon className="w-10 h-10 text-yellow-300" />
       <p className="text-center mt-4 text-white text-base md:text-lg lg:text-xl font-semibold text-opacity-80">
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis 
-  quisquam sint consequatur esse architecto earum laboriosam sapiente 
-  dolorem similique officia eius amet est ducimus nemo maxime consequuntur 
-  atque, iusto tenetur
+  Imagine.AI is honestly such a fun and impressive tool. I just type a few words, and the AI instantly turns it into stunning visuals. Whether I need futuristic artwork or fantasy landscapes, it delivers every time. As someone with zero design skills, this website makes me feel like a pro artist. Plus, the interface is super clean and easy to use. Highly recommended for anyone who loves experimenting with AI art!
 </p>
 <div className="mt-8">
   <h1 className="text-base md:text-2xl font-bold text-white text-center">
