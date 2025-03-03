c1c6b3be65d48cab63a1dc5022b8806ceb44af1a
Utkarsh Rana <104561101+utkarshranaa@users.noreply.github.com>
2025-03-03 12:27:35 -0500

    updated SectionHeading.tsx

--- components/Helper/SectionHeading.tsx
+++ components/Helper/SectionHeading.tsx
@@ -9,8 +9,7 @@ const SectionHeading = ({heading}:Props) => {
     <div className='w-[80%] m-auto'>
         <h1 className='text-2xl lg:text-3xl font-bold text-white'>{heading}</h1>
         <p className='text-sm sm:text-base md:text-lg font-medium mt-2 text-gray-200'>
-            Lorem ipsum dolor amie, nijadaddy madde this shit
-            kya bolti public?
+            Take a peek at our latest AI-generated visuals—each one created from simple text prompts using our advanced AI engine.
         </p>
     </div>
   )
